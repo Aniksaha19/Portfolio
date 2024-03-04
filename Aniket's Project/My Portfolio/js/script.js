3b16bda656e6b3c473fecfaf77f582ee879c365d
Aniksaha19 <129836871+Aniksaha19@users.noreply.github.com>
2024-03-04 19:51:45 +0530

    bug fix

---
+++ Aniket's Project/My Portfolio/js/script.js
@@ -0,0 +1,6 @@
+typed = new Typed('#changing-text', {
+    strings: ['Web Designer', 'Frontend Developer'],
+    typeSpeed: 70,
+    backSpeed: 0,
+    loop: true,
+  });
--- Aniket's Project/My Portfolio/script.js
+++
@@ -1,6 +0,0 @@
-typed = new Typed('#changing-text', {
-    strings: ['Web Designer', 'Frontend Developer'],
-    typeSpeed: 70,
-    backSpeed: 0,
-    loop: true,
-  });
